8b9a96abb5d0a2a377c4cd872d057eb9c6524f2c
Rafael Niebles <rnieblesealo@gmail.com>
2025-02-01 13:45:10 -0500

    save

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-# this is a python file!
+import torch
+import torch.nn as neural_network
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+
+
+input_dimension = 468  # Amount of mediapipe landmarks
+
+layer_1_size = 64
+layer_2_size = 64
+
+
+class LandmarkEmotionModel(neural_network.Module):
+    def __init__(self):
+        super().init()
+
+        self.layer1 = neural_network.Linear(input_dimension, layer_1_size)
